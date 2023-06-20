3122f69878ee66eea144b3a3d5fb73eb720bb09b
MS <72651078+maddydevgits@users.noreply.github.com>
2023-06-20 14:16:06 +0530

    Create register.sol

---
+++ activity7/contracts/register.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract register{
+    
+    string[] _names;
+    string[] _emailids;
+    string[] _mobiles;
+    string[] _passwords;
+    string[] _usernames;
+
+    function signup(string memory name,string memory emailid,string memory mobile,string memory password,string memory username) public{
+        _names.push(name);
+        _emailids.push(emailid);
+        _mobiles.push(mobile);
+        _passwords.push(password);
+        _usernames.push(username);
+    }
+
+    function display() public view returns(string[] memory,string[] memory,string[] memory,string[] memory,string[] memory) {
+        return(_names,_emailids,_mobiles,_passwords,_usernames);
+    }
+}
